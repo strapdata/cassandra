05d861dabee3820bc8629d9ee42aee8ae21a9b0f
Vincent Royer <vroyer@strapdata.com>
2017-04-16 23:13:09 +0200

    CASSANDRA-13269 Snapshot support for custom secondary indices.
    Add a default implementation for getSnapshotWithoutFlushTask(String snapshotName).

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -1640,6 +1640,9 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean
                     writeSnapshotSchema(snapshotName);
             }
         }
+        
+        this.indexManager.snapshotWithoutFlush(snapshotName);
+        
         if (ephemeral)
             createEphemeralSnapshotMarkerFile(snapshotName);
         return snapshottedSSTables;
