751e46987d4af2921831fad2a1b3a05f8501d9d5
Vincent Royer <vroyer@strapdata.com>
2017-10-30 22:41:13 +0100

    Upgrade hppc library to version 0.7.1

--- src/java/org/apache/cassandra/index/sasi/analyzer/StandardAnalyzer.java
+++ src/java/org/apache/cassandra/index/sasi/analyzer/StandardAnalyzer.java
@@ -27,17 +27,21 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.cassandra.index.sasi.analyzer.filter.*;
 import org.apache.cassandra.db.marshal.AbstractType;
 import org.apache.cassandra.db.marshal.AsciiType;
 import org.apache.cassandra.db.marshal.UTF8Type;
+import org.apache.cassandra.index.sasi.analyzer.filter.BasicResultFilters;
+import org.apache.cassandra.index.sasi.analyzer.filter.FilterPipelineBuilder;
+import org.apache.cassandra.index.sasi.analyzer.filter.FilterPipelineExecutor;
+import org.apache.cassandra.index.sasi.analyzer.filter.FilterPipelineTask;
+import org.apache.cassandra.index.sasi.analyzer.filter.StemmingFilters;
+import org.apache.cassandra.index.sasi.analyzer.filter.StopWordFilters;
 import org.apache.cassandra.io.util.DataInputBuffer;
 import org.apache.cassandra.utils.ByteBufferUtil;
 
-import com.google.common.annotations.VisibleForTesting;
-
+import com.carrotsearch.hppc.IntObjectHashMap;
 import com.carrotsearch.hppc.IntObjectMap;
-import com.carrotsearch.hppc.IntObjectOpenHashMap;
+import com.google.common.annotations.VisibleForTesting;
 
 public class StandardAnalyzer extends AbstractAnalyzer
 {
@@ -61,7 +65,7 @@ public class StandardAnalyzer extends AbstractAnalyzer
         KATAKANA(12),
         HANGUL(13);
 
-        private static final IntObjectMap<TokenType> TOKENS = new IntObjectOpenHashMap<>();
+        private static final IntObjectMap<TokenType> TOKENS = new IntObjectHashMap<>();
 
         static
         {
