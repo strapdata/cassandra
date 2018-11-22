7ce58dec8a0ead44c19b52eeb443439c9053cf54
Vincent Royer <vroyer@strapdata.com>
2018-11-22 17:07:35 +0100

    Add support for CQL schema update transaction

--- src/java/org/apache/cassandra/service/MigrationListener.java
+++ src/java/org/apache/cassandra/service/MigrationListener.java
@@ -19,17 +19,45 @@ package org.apache.cassandra.service;
 
 import java.util.List;
 
+import org.apache.cassandra.config.CFMetaData;
 import org.apache.cassandra.db.marshal.AbstractType;
+import org.apache.cassandra.schema.KeyspaceMetadata;
 
 public abstract class MigrationListener
 {
+	/**
+	 * Start a schema merge transaction.
+	 * This helps to manage a batch of schema mutations together.
+	 */
+	public void onBeginTransaction()
+	{
+	}
+
+	/**
+	 * End a schema merge transaction.
+	 */
+	public void onEndTransaction()
+	{
+	}
+
+    public void onCreateKeyspace(KeyspaceMetadata ksm)
+    {
+    	onCreateKeyspace(ksm.name);
+    }
+
     public void onCreateKeyspace(String ksName)
     {
+    }
+
+    public void onCreateColumnFamily(KeyspaceMetadata ksm, CFMetaData cfm)
+    {
+    	onCreateColumnFamily(ksm.name, cfm.cfName);
     }
 
     public void onCreateColumnFamily(String ksName, String cfName)
     {
     }
+
 
     public void onCreateView(String ksName, String viewName)
     {
@@ -48,12 +76,22 @@ public abstract class MigrationListener
     {
     }
 
+    public void onUpdateKeyspace(KeyspaceMetadata ksm)
+    {
+    	onUpdateKeyspace(ksm.name);
+    }
+
     public void onUpdateKeyspace(String ksName)
     {
     }
 
     // the boolean flag indicates whether the change that triggered this event may have a substantive
     // impact on statements using the column family.
+    public void onUpdateColumnFamily(KeyspaceMetadata ksm, CFMetaData cfm, boolean affectsStatements)
+    {
+    	onUpdateColumnFamily(ksm.name, cfm.cfName, affectsStatements);
+    }
+
     public void onUpdateColumnFamily(String ksName, String cfName, boolean affectsStatements)
     {
     }
@@ -75,8 +113,18 @@ public abstract class MigrationListener
     {
     }
 
+    public void onDropKeyspace(KeyspaceMetadata ksm)
+    {
+    	onDropKeyspace(ksm.name);
+    }
+
     public void onDropKeyspace(String ksName)
     {
+    }
+
+    public void onDropColumnFamily(KeyspaceMetadata ksm, CFMetaData cfm)
+    {
+    	onDropColumnFamily(ksm.name, cfm.cfName);
     }
 
     public void onDropColumnFamily(String ksName, String cfName)
